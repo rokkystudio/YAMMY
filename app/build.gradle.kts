d557de6b9a0394b8d23a97e2a0dd7fc584bdfaf5
rokkystudio <rokkystudio@gmail.com>
2026-09-22 16:45:35 +0300

    YAMMY 1.0.8 refine light theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.rokkystudio.yammy"
         minSdk = 24
         targetSdk = 37
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.0.8"
     }
 
     buildTypes {
