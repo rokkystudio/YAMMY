4dc75b24775b020b6920a4dc283eec03a2189f92
rokkystudio <rokkystudio@gmail.com>
2026-09-22 16:42:44 +0300

    YAMMY 1.0.7 normalize table formatting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.rokkystudio.yammy"
         minSdk = 24
         targetSdk = 37
-        versionCode = 6
-        versionName = "1.0.6"
+        versionCode = 7
+        versionName = "1.0.7"
     }
 
     buildTypes {
